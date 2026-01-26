0e634ab4d59a8b44239d1cbe54a4d5667621da2f
jormg34587 <jorge.martin139@educa.madrid.org>
2026-01-26 12:20:34 +0100

    CreateSQL clientes

--- src/main/java/practicajasperreport/createSQL_almacenInterfaces.sql
+++ src/main/java/practicajasperreport/createSQL_almacenInterfaces.sql
@@ -1,3 +1,13 @@
 CREATE TABLE clientes(
-    
+codigo INT,
+nif VARCHAR(9),
+apellidos VARCHAR(35),
+nombre VARCHAR(15),
+domicilio VARCHAR(40),
+codigo_postal VARCHAR(5),
+localidad VARCHAR(20),
+telefono VARCHAR(9),
+fax VARCHAR(9),
+email VARCHAR(20),
+total_ventas FLOAT
 );
